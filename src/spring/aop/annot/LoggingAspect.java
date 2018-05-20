f315a7eae4168c3e7e53e548ca7fbc611d0b3b12
KyuYoung <nagaraz07@gmail.com>
2018-05-20 10:36:29 +0900

    kyu

---
+++ src/spring/aop/annot/LoggingAspect.java
@@ -0,0 +1,40 @@
+package spring.aop.annot;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+	@Before("execution(public * springstudy..*(..))")
+	public void before(JoinPoint joinpoint) {
+		String signatureString = joinpoint.getSignature().getName();
+		for (Object arg : joinpoint.getArgs())
+			System.out.println("@Before [ " + signatureString + " ] 아규먼트" + arg);
+	}
+	@AfterReturning(pointcut="execution(public * springstudy.user.service.*.*(..))",returning="ret")
+	public void afterReturning(JoinPoint joinPoint , Object ret) {
+		String signatureString = joinPoint.getSignature().getName();
+		System.out.println("@AfterReturning [ "+signatureString + " ] 메서드 실행 후처리 수행");
+		System.out.println("@AfterReturning [ "+signatureString + " ] 리턴값 ="+ ret);
+		
+	}
+	@AfterThrowing(pointcut="execution(* *..UserService*.*(..))",throwing="ex")
+	public void afterThrowing(JoinPoint joinPoint, Throwable ex) {
+		String signatureString = joinPoint.getSignature().getName();
+		System.out.println("@AfterReturning [ "+signatureString + " ] 메서드 실행 중 예외 발생");
+		System.out.println("@AfterReturning [ "+signatureString + " ] 예외 ="+ ex.getMessage());
+	}
+	
+	@After("execution(* *..*.*User(..))")
+	public void afterFinally(JoinPoint joinPoint) {
+		String signatureString = joinPoint.getSignature().getName();
+		System.out.println("@After [ "+signatureString+" ] 메서드 실행 완료");
+	}
+	
+}
